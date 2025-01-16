2cb574f996a8fc88f84255fc1e1ef8ec19c2a531
Martin Eutsey <meutsey8828@floridapoly.edu>
2025-01-16 13:29:48 +0000

    Code first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,21 @@
+// This is the first program in C.
+// It prints a welcome message to the screen.
+
+// - one-line comment
+/*
+
+This is
+a multiline comment.
+
+*/
+
+#include <stdio.h>
+
+// Create a main function.
+int main() {
+    // Call the printf function.
+    printf("Welcome\nto\nCOP2271!\n"); // '\n' - new line, also called an escape sequence.
+    printf("I\nwant\nto\ncode!\n");
+
+    return 0; // return 0 - exits the main function. add everytime at the end of a main function.
+}
