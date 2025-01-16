e34771acffdc2d224ee0d44f47b9e43634b80f9e
Martin Eutsey <meutsey8828@floridapoly.edu>
2025-01-16 13:46:19 +0000

    Practiced printing integers

---
+++ 03_student_info/student_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int course_id = 2271;
+    int student_id = 8828;
+    int sum = student_id - course_id;
+    printf("Name: Martin Eutsey\n");
+    printf("Course ID: %d\n", course_id);
+    printf("Student ID: %d\n", student_id);
+    printf("Student ID - Course ID is: %d\n", sum);
+
+    return 0;
+}
