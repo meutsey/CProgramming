3b74798c543eae86c51114947813a2075589dd45
Martin Eutsey <meutsey8828@floridapoly.edu>
2025-01-16 14:42:13 +0000

    Converts an input age in years to minutes

---
+++ 05_years_to_minutes/years_to_minutes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    int minutes;
+    puts("Enter your age in years: "); // Prints to console
+    scanf("%d", &age); // Wants a number input in the console and stores it to the specified variable
+    minutes = age * 365 * 24 * 60;
+    printf("%d years is %d minutes\n", age, minutes); 
+   
+    return 0;
+}
